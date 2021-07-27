475ded25f6693f41eee240fcf170c048a081ecc3
Sambit_Sankalp <sambit.sankalp.official@gmail.com>
2021-07-27 11:30:14 +0530

    reviews component added

--- frontend/src/components/FullArticle/FullArticle.jsx
+++ frontend/src/components/FullArticle/FullArticle.jsx
@@ -1,23 +1,15 @@
 import React, {useState,useEffect} from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from '@material-ui/core/Button';
-import clsx from 'clsx';
-import InputLabel from '@material-ui/core/InputLabel';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControl from '@material-ui/core/FormControl';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import { useDispatch, useSelector} from 'react-redux';
 import Typography from "@material-ui/core/Typography";
-import { Paper } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Alert from '@material-ui/lab/Alert';
 import Grid from "@material-ui/core/Grid";
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import {listArticleDetails, createArticleReview} from "../../actions/articlesAction";
-import {ARTICLE_CREATE_COMMENT_RESET} from '../../constants/articleConstants'
+import Reviews from './Reviews';
+import {listArticleDetails} from "../../actions/articlesAction";
 import { useParams } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -33,41 +25,14 @@ const useStyles = makeStyles((theme) => ({
 
 const FullArticle = () => {
   const { id } = useParams()
-  const [comment, setComment] = useState('')
-  const [like, setLike] = useState(false)
   const dispatch = useDispatch()
 
   const articleDetails = useSelector(state => state.articleDetails)
     const { loading , error , article } = articleDetails
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin
-
-    const articleCommentCreate = useSelector(state => state.articleCommentCreate)
-    const { loading: commentLoading , error: commentError , success } = articleCommentCreate
-
-
   useEffect(() => {
-    if(success)
-    {
-      alert('Reviewed Successfully')
-      setComment('')
-      dispatch({type: ARTICLE_CREATE_COMMENT_RESET})
-    }
     dispatch(listArticleDetails(id))
-  }, [dispatch, id, success])
-
-  const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(createArticleReview(id, {comment}))
-    
-  }
-
-  const submitButtonHandler = (e) => {
-    e.preventDefault()
-    dispatch(createArticleReview(id, {like}))
-    
-  }
+  }, [dispatch, id])
 
   const classes = useStyles();
   return (
@@ -147,63 +112,7 @@ const FullArticle = () => {
                 circumstances under which electricity can’t provided to the institute
                 and when we should be wary about the power cuts taking place.
               </Typography>
-              <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-              <form className={classes.root} onSubmit={submitButtonHandler} noValidate autoComplete="off">
-                <Button variant="contained" color="primary" type="submit" onClick={setLike(true)}>
-                  {article.reviews.liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-                </Button>
-              </form>
-              <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-              <div style={{ padding: 14 }} className="App">
-                <h1>Comments</h1>
-                {article.reviews.length === 0 && <Alert severity="success">No Comments</Alert>}
-                <Paper style={{ padding: "40px 20px" }}>
-                  {article.reviews.map((review) => (
-                    <div>
-                      <Grid container wrap="nowrap" spacing={2}>
-                        <Grid justifyContent="left" item xs zeroMinWidth>
-                          <h4 style={{ margin: 0, textAlign: "left" }}>{review.name}</h4>
-                          <p style={{ textAlign: "left" }}>
-                            {review.comment}
-                          </p>
-                        </Grid>
-                      </Grid>
-                      <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
-                    </div>
-                  ))}
-                  <h3>Write your comment</h3>
-                    {commentError && <Alert severity="error">{commentError}</Alert>}
-                    {userInfo ? (
-                      <form className={classes.root} onSubmit={submitHandler} noValidate autoComplete="off">
-                        <div>
-                          <FormControl
-                            fullWidth
-                            className={clsx(classes.margin, classes.textField)}
-                            variant="outlined"
-                          >
-                            <InputLabel htmlFor="outlined-adornment-comment">Comment Here</InputLabel>
-                            <OutlinedInput
-                              id="outlined-adornment-comment"
-                              value={comment}
-                              onChange={(e) => setComment(e.target.value)}
-                              aria-describedby="outlined-name-helper-text"
-                              inputProps={{
-                                "aria-label": "comment"
-                              }}
-                              labelWidth={40}
-                            />
-                          </FormControl>
-                          <Button variant="contained" color="primary" type="submit">
-                            Submit
-                          </Button>
-                        </div>
-                      </form>
-                    ): 
-                    <Alert severity="success">
-                        Please <Link to='/login'>Sign In</Link> to comment{' '}
-                    </Alert>}
-                </Paper>
-              </div>
+              <Reviews id={article._id}/>
             </div>
           )}
       </Container>
